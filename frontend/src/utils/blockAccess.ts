be238696eed0224b51626ac4d0e081d69af65551
Naufal Akbar <kbrnugroho@gmail.com>
2022-11-22 17:59:30 +0700

    upd: spacing

--- frontend/src/utils/blockAccess.ts
+++ frontend/src/utils/blockAccess.ts
@@ -1,6 +1,7 @@
 export const blockAccess = (localItem: string, router: any) => {
   if (typeof window !== 'undefined') {
     const item = JSON.parse(localStorage.getItem(localItem) || '{}');
+
     if (item.level !== localItem) {
       alert('Anda tidak memiliki akses ke halaman ini!');
       localStorage.clear();
