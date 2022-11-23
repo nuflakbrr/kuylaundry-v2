ecd147ca7de91f85b8ee249065769f560cf2e8f4
Naufal Akbar <kbrnugroho@gmail.com>
2022-11-23 17:07:26 +0700

    upd: fixed type data for format `total harga`

--- frontend/src/utils/formatCurrency.ts
+++ frontend/src/utils/formatCurrency.ts
@@ -1,4 +1,4 @@
-export const formatCurrency = (num: string) => {
+export const formatCurrency = (num: any) => {
   return new Intl.NumberFormat('id-ID', {
     style: 'currency',
     currency: 'IDR',
