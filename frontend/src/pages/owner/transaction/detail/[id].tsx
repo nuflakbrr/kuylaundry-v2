6bc898903442acde8df79fa67967af3691f870de
Naufal Akbar <kbrnugroho@gmail.com>
2022-11-29 15:39:48 +0700

    feat: Detail Transaksi for Owner

---
+++ frontend/src/pages/owner/transaction/detail/[id].tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import ContainersTransactionDetails from '../../../../components/Containers/Owner/Transaction/TransactionDetail';
+
+const DetailTransaction: FC = () => {
+  return <ContainersTransactionDetails />;
+};
+
+export default DetailTransaction;
